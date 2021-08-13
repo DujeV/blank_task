d683d985c73ff25154f752c1df922d5f05a4e24b
DujeV <dvidov01@fesb.hr>
2021-08-13 15:57:53 +0200

    git fetch

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,14 @@
-const { REACT_APP_BASE_API, REACT_APP_API_KEY, REACT_APP_API_HASH } = process.env;
+const { REACT_APP_BASE_API, REACT_APP_API_KEY, REACT_APP_API_HASH } =
+  process.env;
 
-export const getCharacters = async () => { 
-    await fetch(REACT_APP_BASE_API + `&apikey=${REACT_APP_API_KEY}&hash=${REACT_APP_API_HASH}`)
-    .then(res => res.json())
-    .then(data => console.log(data)).catch(err => {
-        console.log("Error Reading data " + err);
-      });;
-}
+export const getCharacters = async () => {
+  await fetch(
+    REACT_APP_BASE_API +
+      `&apikey=${REACT_APP_API_KEY}&hash=${REACT_APP_API_HASH}`
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => {
+      console.log("Error Reading data " + err);
+    });
+};
