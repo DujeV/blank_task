f109adc514376096993d9885f8da46ad1b02b334
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-08-13 13:51:42 +0200

    Changed .env and .env.example location

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,1 +1,10 @@
 const { REACT_BASE_API } = process.env;
+
+const makeResponse = async (url) => {
+    const response = await fetch(url);
+    return response.json();
+}
+
+export const getMarvelCharacters = async () => {
+    return makeResponse(REACT_BASE_API + `/v1/public/charactersFetches`)
+}
